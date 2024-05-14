1245a7779545e216f75181936f475e8311f9857d
elvodqa <emircengiz21@gmail.com>
2024-05-14 22:42:47 +0300

    Make it sexier

--- Syndiesis/Controls/Editor/CodeEditor.axaml.cs
+++ Syndiesis/Controls/Editor/CodeEditor.axaml.cs
@@ -670,11 +670,13 @@ public partial class CodeEditor : UserControl
         _editor.InsertText(e.Text);
     }
 
+    private static readonly KeyModifiers ctrlModifier = OperatingSystem.IsMacOS() ? KeyModifiers.Meta : KeyModifiers.Control;
+
     protected override void OnKeyDown(KeyEventArgs e)
     {
         var modifiers = e.KeyModifiers;
-
-        bool hasControl = modifiers.HasFlag(OperatingSystem.IsMacOS() ? KeyModifiers.Meta : KeyModifiers.Control);
+        
+        bool hasControl = modifiers.HasFlag(ctrlModifier);
         bool hasShift = modifiers.HasFlag(KeyModifiers.Shift);
 
         switch (e.Key)
