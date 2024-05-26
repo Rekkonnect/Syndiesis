674c39e9d31b40f0579559c024e165839d2b9d5d
Rekkonnect <kalfas.alex@gmail.com>
2024-05-27 00:14:27 +0300

    Fix URL opening for non-Windows

--- Syndiesis/Utilities/ProcessUtilities.cs
+++ Syndiesis/Utilities/ProcessUtilities.cs
@@ -1,11 +1,40 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Syndiesis.Utilities;
 
 public static class ProcessUtilities
 {
+    // --- https://stackoverflow.com/a/43232486
+    // |
+    //  -> https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/
+    //     "Fun times in this cross-platform world."
     public static void OpenUrl(string url)
     {
-        Process.Start("explorer", url);
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
     }
 }
