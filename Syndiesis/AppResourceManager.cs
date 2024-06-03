bcd2936c495d1babdd58a8bbe1b8393ccdaa944c
Rekkonnect <kalfas.alex@gmail.com>
2024-06-04 00:34:39 +0300

    Prepare to support VB

--- Syndiesis/AppResourceManager.cs
+++ Syndiesis/AppResourceManager.cs
@@ -7,6 +7,8 @@ public class AppResourceManager(App app)
 {
     private readonly App _app = app;
 
+    public Image? LogoCSImage => _app.FindResource("LogoCSImage") as Image;
+    public Image? LogoVBImage => _app.FindResource("LogoVBImage") as Image;
     public Image? PenImage => _app.FindResource("PenImage") as Image;
     public Image? SpinnerImage => _app.FindResource("SpinnerImage") as Image;
     public Image? SuccessImage => _app.FindResource("SuccessImage") as Image;
